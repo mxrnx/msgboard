46e3baa4c741fae0ee881fcb4ab3fe2d2339228a
Marnix Massar <marnixm@q42.nl>
2025-05-10 11:20:46 +0200

    Add icons, tweak forms

--- src/models/thread.ts
+++ src/models/thread.ts
@@ -1,6 +1,20 @@
 import type { BasePost } from "./post";
 
-export const threadIcons = ["🏛️", "🦅", "🔥", "📜", "🇻🇦", "🏺", "🌳"] as const;
+export const threadIcons = [
+  "🏛️",
+  "🔥",
+  "📜",
+  "🏺",
+  "🌳",
+  "⚔️",
+  "🪉",
+  "⚖️",
+  "🌾",
+  "🎭",
+  "🐎",
+  "⛲",
+  "🌍",
+] as const;
 export type ThreadIcon = (typeof threadIcons)[number];
 
 export type Thread = BasePost & {
